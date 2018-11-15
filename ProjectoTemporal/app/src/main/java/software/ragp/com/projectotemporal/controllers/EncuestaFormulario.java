36cde5c1f8c927908c2ca9a2ab477c89a71a20af
RodrigoAGuspian <rodseiya20008@gmail.com>
2018-11-15 01:46:02 -0500

    asd

--- ProjectoTemporal/app/src/main/java/software/ragp/com/projectotemporal/controllers/EncuestaFormulario.java
+++ ProjectoTemporal/app/src/main/java/software/ragp/com/projectotemporal/controllers/EncuestaFormulario.java
@@ -73,9 +73,8 @@ public class EncuestaFormulario extends AppCompatActivity {
     }
 
     private void finalizarTodo() {
-
         RiesgoFormulario.riesgoFormulario.finish();
-
+        TipoDeProducto.tipoDeProducto.finish();
         finish();
     }
 
