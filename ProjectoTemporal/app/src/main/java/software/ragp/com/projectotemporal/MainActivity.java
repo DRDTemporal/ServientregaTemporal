fe3dadb8459f63b1f3753fb861f811b4b26814ae
DavidPyn27 <popayan159@gmail.com>
2018-11-14 20:25:07 -0500

    fondoApp

--- ProjectoTemporal/app/src/main/java/software/ragp/com/projectotemporal/MainActivity.java
+++ ProjectoTemporal/app/src/main/java/software/ragp/com/projectotemporal/MainActivity.java
@@ -2,6 +2,9 @@ package software.ragp.com.projectotemporal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle("Inicio");
+        getSupportActionBar().setIcon(R.drawable.inicio);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
